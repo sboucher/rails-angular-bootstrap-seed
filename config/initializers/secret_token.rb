34aa6275541006798ab0df819f9731da6e064070
Olivier Colot <olivier.colot@movup.me>
2013-05-24 08:31:10 +0200

    rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsAngularBootstrapSeed::Application.config.secret_key_base = '283ca558a88192423086a67e4fa782b7a4283d3681a09cb69ae42a80d8aa7d28febbb744d914f12d975e511e112c53c7b078eb0da687839c98886e59b4570035'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsAngularBootstrapSeed::Application.config.session_store :cookie_store, key: '_rails-angular-bootstrap-seed_session'
